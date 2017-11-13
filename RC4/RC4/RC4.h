3d9365da4664e136a1c798f298ec3f977b87306b
CGlacia <crystalglacia@gmail.com>
2017-11-12 23:28:51 -0500

    most probably done
    just needs debugging and a report

---
+++ RC4/RC4/RC4.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+void EncryptDecrypt(string, string, string);
+char GetStateByte();
+void input(string);
