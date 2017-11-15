02a6a5d7f6cbbb009aae0115d3a7875338a2bef4
CGlacia <crystalglacia@gmail.com>
2017-11-15 00:37:58 -0500

    more analysis

--- RC4/RC4Analysis/RC4Analysis.cpp
+++ RC4/RC4Analysis/RC4Analysis.cpp
@@ -24,20 +24,23 @@ int main()
 	cout << "Single-character (1), or digram (2) analysis?" << endl;
 	cin >> type;
 
-	fstream input;
-	input.open(filename.c_str(), ios::in | ios::binary);
-	if (input.is_open())
+	if (type == 1) //single character analysis
 	{
-		char firstch, seconch;
+		fstream input;
+		input.open(filename.c_str(), ios::in | ios::binary);
+
+		char firstch;
 		bool foundGram = false, needSecond = true;
 
-		if (type == 1) //single character analysis
+		if (input.is_open())
 		{
 			input.get(firstch);
 
 			while (!input.eof())
 			{
-				for (int i = 0; i < grams.size(); i++)
+				int gSize = grams.size();
+
+				for (int i = 0; i < gSize; i++)
 				{
 					if (grams[i].text == string(1, firstch))
 					{
@@ -59,16 +62,33 @@ int main()
 				input.get(firstch);
 			}
 		}
-		else if (type == 2)
+		else
+		{
+			cout << "Input couldn't be opened." << endl;
+			return 1;
+		}
+
+		input.close();
+	}
+	else if (type == 2)
+	{
+		fstream input;
+		input.open(filename.c_str(), ios::in | ios::binary);
+
+		char firstch, seconch;
+		bool foundGram = false, needSecond = true;
+
+		if (input.is_open())
 		{
 			input.get(firstch);
 
 			while (!input.eof())
 			{
 				input.get(seconch);
-				string thisGram = firstch + seconch + "";
+				string thisGram = string(1, firstch) + string(1, seconch);
+				int gSize = grams.size();
 
-				for (int i = 0; i < grams.size(); i++)
+				for (int i = 0; i < gSize; i++)
 				{
 					if (grams[i].text == thisGram)
 					{
@@ -85,18 +105,19 @@ int main()
 					newGram.count = 1;
 					grams.push_back(newGram);
 				}
-				
+
 				foundGram = false;
+				firstch = seconch;
 			}
 		}
-	}
-	else
-	{
-		cout << "Input file couldn't be opened." << endl;
-		return 1;
-	}
+		else
+		{
+			cout << "Input couldn't be opened." << endl;
+			return 1;
+		}
 
-	input.close();
+		input.close();
+	}
 
 	ofstream output;
 	output.open(outFile.c_str(), ios::out | ios::binary);
@@ -116,6 +137,8 @@ int main()
 	}
 
 	output.close();
+
+	cout << "Analysis complete! Check analysis.txt." << endl;
 
     return 0;
 }
